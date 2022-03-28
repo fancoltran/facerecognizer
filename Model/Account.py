1d7e2333222564be5be4ec3ebc0c2fd00885b039
ngoc <ngoctt@vhv.vn>
2022-03-28 12:24:55 +0700

    Update vector

--- Model/Account.py
+++ Model/Account.py
@@ -75,8 +75,8 @@ class Account:
                 row = row.replace(']]', '')
                 row = [float(num) for num in row.split(", ")]
                 results.append(row)
-            dictionary.update({key + "_" + _id + "_1": results[-1]})
-            dictionary.update({key + "_" + _id + "_0": results[0:7])
+            dictionary.update({key + "_" + _id + "_1": results[0]})
+            dictionary.update({key + "_" + _id + "_0": results[1:]})
 
         return dictionary
 
